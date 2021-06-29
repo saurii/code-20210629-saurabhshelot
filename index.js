c3592c579aa6fd10bb68d992ae431232a460f474
Saurabh Shelot <sshelot00@gmail.com>
2021-06-29 17:37:14 +0530

    Added some error handling

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ let personjson = [{ "Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, {
 }, { "Gender": "Female", "HeightCm": 150, "WeightKg": 70 }, { "Gender": "Female", "HeightCm": 167, "WeightKg": 82 }, { "Gender": "Male", "HeightCm": 165, "WeightKg": 75 }, { "Gender": "Male", "HeightCm": 168, "WeightKg": 88 }, { "Gender": "Female", "HeightCm": 157, "WeightKg": 65 }]
 
 function get_bmi_details(person_data) {
-    if (person_data && person_data.length > 0) {
-        person_data.map((personElement) => {
-            let bmidata = get_bmi_risk_category(personElement.HeightCm / 100, personElement.WeightKg);
-            personElement['Bmi'] = Number(bmidata.bmi);
-            personElement['Category'] = bmidata.category;
-            personElement["Risk"] = bmidata.risk;
-        });
-        return person_data
-    } else {
-        return 'provide valid json array';
+    try {
+        if (person_data && person_data.length > 0) {
+            person_data.map((personElement) => {
+                let bmidata = get_bmi_risk_category(personElement.HeightCm / 100, personElement.WeightKg);
+                personElement['Bmi'] = Number(bmidata.bmi);
+                personElement['Category'] = bmidata.category;
+                personElement["Risk"] = bmidata.risk;
+            });
+            return person_data
+        } else {
+            return 'provide valid json array';
+        }
+    } catch (err) {
+        // need to add logging
+        return false
     }
-
 }
 
 function get_bmi_risk_category(height, weight) {
@@ -36,4 +40,8 @@ function get_bmi_risk_category(height, weight) {
 let bmi_details = get_bmi_details(personjson);
 if (bmi_details && bmi_details.length > 0 && !bmi_details === 'provide valid json array') {
     let totalcategorywisecount = get_categorywise_count(bmi_details);
+} else if (!bmi_details) {
+    console.error('Error occured')
+} else {
+    console.log('provide valid json array')
 }
