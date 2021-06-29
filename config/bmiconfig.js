56d3caa9d99f5bc8d0bc40e2780ce884046a40d9
Saurabh Shelot <sshelot00@gmail.com>
2021-06-29 17:22:34 +0530

    Added risk configuration

--- config/bmiconfig.js
+++ config/bmiconfig.js
@@ -4,22 +4,22 @@ const config = require('./config');
 module.exports.get_risk_category_data = (bmi) => {
     return bmi <= 18.4 ? {
         category: config.bmi_categories['underweight'],
-        risk: 'Malnutrition risk'
+        risk: config.bmi_risk['malnutrition_risk']
     } : bmi >= 18.5 && bmi <= 24.9 ? {
         category: config.bmi_categories['normalweight'],
-        risk: 'Low risk'
+        risk: config.bmi_risk['low_risk']
     } : bmi >= 25 && bmi <= 29.9 ? {
         category: config.bmi_categories['overweight'],
-        risk: 'Enhanced risk'
+        risk: config.bmi_risk['enhanced_risk']
     } : bmi >= 30 && bmi <= 34.9 ? {
         category: config.bmi_categories['moderately_obese'],
-        risk: 'Medium risk'
+        risk: config.bmi_risk['medium_risk']
     } : bmi >= 35 && bmi <= 39.9 ? {
         category: config.bmi_categories['severely_obese'],
-        risk: 'High risk'
+        risk: config.bmi_risk['high_risk']
     } : bmi >= 40 ? {
         category: config.bmi_categories['very_severely_obese'],
-        risk: 'Very high risk'
+        risk: config.bmi_risk['very_high_risk']
     } : null;
 }
 
--- config/config.js
+++ config/config.js
@@ -7,4 +7,14 @@ let bmi_categories = {
     very_severely_obese: 'Very severely obese'
 }
 
+let bmi_risk = {
+    malnutrition_risk: 'Malnutrition risk',
+    low_risk: 'Low risk',
+    enhanced_risk: 'Enhanced risk',
+    medium_risk: 'Medium risk',
+    high_risk: 'High risk',
+    very_high_risk: 'Very high risk'
+}
+
 module.exports.bmi_categories = bmi_categories;
+module.exports.bmi_risk = bmi_risk;
